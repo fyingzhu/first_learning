7c6c2f133554dbf287da4d0af107c0347964c02c
fyingzhu <fyingzhu@126.com>
2017-05-27 16:14:11 +0800

    fyingzhu

---
+++ temp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+num = []        
+i = 2
+for i in range(2,100):
+    j = 2
+    for j in range(2,i):
+        if(i%j==0):
+            break
+        else:
+          num.append(i)
+    print num
